aac6c2644d5a8f1509e7d1da148578e50e56fd1d
Tim te Beek <tim@moderne.io>
2024-08-09 16:20:30 +0200

    Also update test package

--- src/test/java/org/openrewrite/reactive/reactor/ReactorFluxPublishNextToShareNextTest.java
+++ src/test/java/org/openrewrite/reactive/reactor/ReactorFluxPublishNextToShareNextTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.reactor;
+package org.openrewrite.reactive.reactor;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;

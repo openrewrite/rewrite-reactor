08fb473dbb995457dcf1a8df9badf8fdafba16cf
Tim te Beek <tim@moderne.io>
2024-08-09 11:32:28 +0200

    Initial version

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.openrewrite.build.recipe-library") version "latest.release"
+}
+
+group = "org.openrewrite.recipe"
+description = "Reactor Migration"
+
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
+dependencies {
+    implementation(platform("org.openrewrite:rewrite-bom:$rewriteVersion"))
+    implementation("org.openrewrite:rewrite-java")
+    implementation("org.openrewrite.recipe:rewrite-java-dependencies:$rewriteVersion")
+    implementation("org.openrewrite:rewrite-templating:$rewriteVersion")
+
+    annotationProcessor("org.openrewrite:rewrite-templating:$rewriteVersion")
+    compileOnly("com.google.errorprone:error_prone_core:2.19.1") {
+        exclude("com.google.auto.service", "auto-service-annotations")
+    }
+
+    testImplementation("org.openrewrite:rewrite-java-17")
+    testImplementation("org.openrewrite:rewrite-test")
+    testImplementation("org.openrewrite:rewrite-maven")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
+}
+
+recipeDependencies {
+    parserClasspath("io.projectreactor:reactor-core:3.4.39")
+}

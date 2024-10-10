e8f00313822e1383f851924e5bc98dab9d21440f
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-10-10 10:58:30 +0200

    Add test coverage WIP

--- src/main/java/org/openrewrite/reactive/reactor/ReactorDoAfterSuccessOrErrorToTap.java
+++ src/main/java/org/openrewrite/reactive/reactor/ReactorDoAfterSuccessOrErrorToTap.java
@@ -25,14 +25,25 @@ import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.FindMethods;
-import org.openrewrite.java.tree.*;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.Statement;
+import org.openrewrite.java.tree.TypeUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 public class ReactorDoAfterSuccessOrErrorToTap extends Recipe {
 
+    private static final String DEFAULT_SIGNAL_LISTENER = "reactor.core.observability.DefaultSignalListener";
+    private static final String SIGNAL_TYPE = "reactor.core.publisher.SignalType";
+
     private static final MethodMatcher DO_AFTER_SUCCESS_OR_ERROR = new MethodMatcher("reactor.core.publisher.Mono doAfterSuccessOrError(..)");
+    private static final MethodMatcher DEFAULT_SIGNAL_LISTENER_DO_FINALLY = new MethodMatcher("* doFinally(..)");
+    private static final MethodMatcher DEFAULT_SIGNAL_LISTENER_DO_ON_ERROR = new MethodMatcher("* doOnError(..)");
+    private static final MethodMatcher DEFAULT_SIGNAL_LISTENER_DO_ON_NEXT = new MethodMatcher("* doOnNext(..)");
 
     @Override
     public String getDisplayName() {
@@ -60,7 +71,7 @@ public class ReactorDoAfterSuccessOrErrorToTap extends Recipe {
                                       "        }\n" +
                                       "\n" +
                                       "        @Override\n" +
-                                      "        public void doOnNext(#{any()} " + doAfterSuccessOrErrorLambdaParams.get(0).getVariables().get(0).getSimpleName() + ") {\n" +
+                                      "        public void doOnNext(" + monoType.getClassName() + " " + doAfterSuccessOrErrorLambdaParams.get(0).getVariables().get(0).getSimpleName() + ") {\n" +
                                       "            // this will be replaced\n" +
                                       "        }\n" +
                                       "\n" +
@@ -73,21 +84,46 @@ public class ReactorDoAfterSuccessOrErrorToTap extends Recipe {
                     J.MethodInvocation replacement = JavaTemplate
                             .builder(template)
                             .contextSensitive()
-                            .imports("reactor.core.observability.DefaultSignalListener", "reactor.core.publisher.Mono", "reactor.core.publisher.SignalType")
-                            .doAfterVariableSubstitution(System.out::println)
+                            .imports(DEFAULT_SIGNAL_LISTENER, SIGNAL_TYPE)
                             .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "reactor-core-3.5.+"))
                             .build()
-                            .apply(getCursor(), mi.getCoordinates().replace(),
-                                    mi.getSelect(),
-                                    TypeTree.build(monoType.getClassName()).withType(monoType));
+                            .apply(getCursor(), mi.getCoordinates().replace(), mi.getSelect());
+                    maybeAddImport(DEFAULT_SIGNAL_LISTENER);
+                    maybeAddImport(SIGNAL_TYPE);
 
                     // These are the statements of the `doAfterSuccessOrError` BiConsumer lambda body
                     List<Statement> doAfterSuccesOrErrorStatements = ((J.Block) ((J.Lambda) mi.getArguments().get(0)).getBody()).getStatements();
+                    J.Identifier resultIdentifier = doAfterSuccessOrErrorLambdaParams.get(0).getVariables().get(0).getName();
+                    List<Statement> resultStatements = new ArrayList<>();
+                    J.Identifier errorIdentifier = doAfterSuccessOrErrorLambdaParams.get(1).getVariables().get(0).getName();
+                    List<Statement> errorStatements = new ArrayList<>();
+                    List<Statement> unidentifiedStatements = new ArrayList<>();
+                    for (Statement olStmt : doAfterSuccesOrErrorStatements) {
+                        if (usesIdentifier(olStmt, resultIdentifier)) {
+                            resultStatements.add(olStmt);
+                        } else if (usesIdentifier(olStmt, errorIdentifier)) {
+                            errorStatements.add(olStmt);
+                        } else {
+                            unidentifiedStatements.add(olStmt);
+                        }
+                    }
                     mi = replacement.withArguments(ListUtils.map(replacement.getArguments(), arg -> {
                         if (arg instanceof J.Lambda && ((J.Lambda) arg).getBody() instanceof J.NewClass) {
                             arg = ((J.Lambda) arg).withBody(((J.NewClass) ((J.Lambda) arg).getBody()).withBody(((J.NewClass) ((J.Lambda) arg).getBody()).getBody().withStatements(ListUtils.map(((J.NewClass) ((J.Lambda) arg).getBody()).getBody().getStatements(), stmt -> {
                                 // here we have access to the method declarations inside the `DefaultSignalListener` inside the tap operator
                                 // We could check the `doAfterSuccessOrError` statements and based on if they use a certain identifier place them in the correct method's body
+                                if (stmt instanceof J.MethodDeclaration) {
+                                    J.ClassDeclaration cd = getCursor().firstEnclosing(J.ClassDeclaration.class);
+                                    if (DEFAULT_SIGNAL_LISTENER_DO_FINALLY.matches((J.MethodDeclaration) stmt, cd)) {
+                                        stmt = ((J.MethodDeclaration) stmt).withBody(((J.MethodDeclaration) stmt).getBody().withStatements(unidentifiedStatements));
+                                    }
+                                    if (DEFAULT_SIGNAL_LISTENER_DO_ON_NEXT.matches((J.MethodDeclaration) stmt, cd)) {
+                                        stmt = ((J.MethodDeclaration) stmt).withBody(((J.MethodDeclaration) stmt).getBody().withStatements(resultStatements));
+                                    }
+                                    if (DEFAULT_SIGNAL_LISTENER_DO_ON_ERROR.matches((J.MethodDeclaration) stmt, cd)) {
+                                        stmt = ((J.MethodDeclaration) stmt).withBody(((J.MethodDeclaration) stmt).getBody().withStatements(errorStatements));
+                                    }
+                                }
                                 return stmt;
                             }))));
                         }
@@ -95,6 +131,21 @@ public class ReactorDoAfterSuccessOrErrorToTap extends Recipe {
                     }));
                 }
                 return mi;
+            }
+
+            private boolean usesIdentifier(Statement olStmt, J.Identifier ident) {
+                AtomicBoolean usesIdentifier = new AtomicBoolean(false);
+                new JavaIsoVisitor<AtomicBoolean>() {
+
+                    @Override
+                    public J.Identifier visitIdentifier(J.Identifier identifier, AtomicBoolean usesIdentifier) {
+                        if (ident.getFieldType().equals(identifier.getFieldType()) && TypeUtils.isOfType(identifier.getType(), ident.getType()) && identifier.getSimpleName().equals(ident.getSimpleName())) {
+                            usesIdentifier.set(true);
+                        }
+                        return identifier;
+                    }
+                }.visit(olStmt, usesIdentifier);
+                return usesIdentifier.get();
             }
         });
     }
--- src/test/java/org/openrewrite/reactive/reactor/ReactorDoAfterSuccessOrErrorToTapTest.java
+++ src/test/java/org/openrewrite/reactive/reactor/ReactorDoAfterSuccessOrErrorToTapTest.java
@@ -70,8 +70,318 @@ class ReactorDoAfterSuccessOrErrorToTapTest implements RewriteTest {
                                 }
             
                                 @Override
-                                public void doOnNext(String value) {
-                                    System.out.println("success" + value);
+                                public void doOnNext(String result) {
+                                    System.out.println("success" + result);
+                                }
+            
+                                @Override
+                                public void doOnError(Throwable error) {
+                                    System.out.println("error" + error);
+                                }
+                            }
+                    ).subscribe();
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void refactorRandomStatementOrder() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+            import reactor.core.publisher.Mono;
+
+            class SomeClass {
+                void doSomething(Mono<String> mono){
+                    mono.doAfterSuccessOrError((result, error) -> {
+                        doSomething();
+                        if (error != null) {
+                            System.out.println("error" + error);
+                            doSomething(error);
+                        } else {
+                            System.out.println("success" + result);
+                            doSomething(result);
+                        }
+                        System.out.println("other logs");
+                    }).subscribe();
+                }
+
+                void doSomething(){
+                }
+
+                void doSomething(Throwable error){
+                }
+
+                void doSomething(String value){
+                }
+            }
+            """,
+            """
+            import reactor.core.observability.DefaultSignalListener;
+            import reactor.core.publisher.Mono;
+            import reactor.core.publisher.SignalType;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono) {
+                    mono.tap(() -> new DefaultSignalListener<>() {
+                                @Override
+                                public void doFinally(SignalType terminationType) {
+                                    doSomething();
+                                    System.out.println("other logs");
+                                }
+            
+                                @Override
+                                public void doOnNext(String result) {
+                                    System.out.println("success" + result);
+                                    doSomething(result);
+                                }
+            
+                                @Override
+                                public void doOnError(Throwable error) {
+                                    System.out.println("error" + error);
+                                    doSomething(error);
+                                }
+                            }
+                    ).subscribe();
+                }
+
+                void doSomething(){
+                }
+
+                void doSomething(Throwable error){
+                }
+
+                void doSomething(String value){
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void invertedIfCheck() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+            import reactor.core.publisher.Mono;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono){
+                    mono.doAfterSuccessOrError((result, error) -> {
+                        if (error == null) {
+                            System.out.println("success" + result);
+                        }
+                        if (null == result) {
+                            System.out.println("error" + error);
+                        }
+                        System.out.println("other logs");
+                    }).subscribe();
+                }
+            }
+            """,
+            """
+            import reactor.core.observability.DefaultSignalListener;
+            import reactor.core.publisher.Mono;
+            import reactor.core.publisher.SignalType;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono) {
+                    mono.tap(() -> new DefaultSignalListener<>() {
+                                @Override
+                                public void doFinally(SignalType terminationType) {
+                                    System.out.println("other logs");
+                                }
+            
+                                @Override
+                                public void doOnNext(String result) {
+                                    System.out.println("success" + result);
+                                }
+            
+                                @Override
+                                public void doOnError(Throwable error) {
+                                    System.out.println("error" + error);
+                                }
+                            }
+                    ).subscribe();
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void multipleIfNoElse() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+            import reactor.core.publisher.Mono;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono){
+                    mono.doAfterSuccessOrError((result, error) -> {
+                        if (error != null) {
+                            System.out.println("error" + error);
+                        }
+                        if (result != null){
+                            System.out.println("success" + result);
+                        }
+                        System.out.println("other logs");
+                    }).subscribe();
+                }
+            }
+            """,
+            """
+            import reactor.core.observability.DefaultSignalListener;
+            import reactor.core.publisher.Mono;
+            import reactor.core.publisher.SignalType;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono) {
+                    mono.tap(() -> new DefaultSignalListener<>() {
+                                @Override
+                                public void doFinally(SignalType terminationType) {
+                                    System.out.println("other logs");
+                                }
+            
+                                @Override
+                                public void doOnNext(String result) {
+                                    System.out.println("success" + result);
+                                }
+            
+                                @Override
+                                public void doOnError(Throwable error) {
+                                    System.out.println("error" + error);
+                                }
+                            }
+                    ).subscribe();
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void multipleIfNoElseRandomStatementOrder() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+            import reactor.core.publisher.Mono;
+
+            class SomeClass {
+                void doSomething(Mono<String> mono){
+                    mono.doAfterSuccessOrError((result, error) -> {
+                        System.out.println("other logs");
+                        if (error != null) {
+                            System.out.println("error" + error);
+                            doSomething(error);
+                        }
+                        doSomething();
+                        if (result != null){
+                            System.out.println("success" + result);
+                            doSomething(result);
+                        }
+                    }).subscribe();
+                }
+
+                void doSomething(){
+                }
+
+                void doSomething(Throwable error){
+                }
+
+                void doSomething(String value){
+                }
+            }
+            """,
+            """
+            import reactor.core.observability.DefaultSignalListener;
+            import reactor.core.publisher.Mono;
+            import reactor.core.publisher.SignalType;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono) {
+                    mono.tap(() -> new DefaultSignalListener<>() {
+                                @Override
+                                public void doFinally(SignalType terminationType) {
+                                    System.out.println("other logs");
+                                    doSomething();
+                                }
+            
+                                @Override
+                                public void doOnNext(String result) {
+                                    System.out.println("success" + result);
+                                    doSomething(result);
+                                }
+            
+                                @Override
+                                public void doOnError(Throwable error) {
+                                    System.out.println("error" + error);
+                                    doSomething(error);
+                                }
+                            }
+                    ).subscribe();
+                }
+
+                void doSomething(){
+                }
+
+                void doSomething(Throwable error){
+                }
+
+                void doSomething(String value){
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void singleIfNoElse() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+            import reactor.core.publisher.Mono;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono){
+                    mono.doAfterSuccessOrError((result, error) -> {
+                        if (error != null) {
+                            System.out.println("error" + error);
+                        }
+                        System.out.println("success" + result);
+                        System.out.println("other logs");
+                    }).subscribe();
+                }
+            }
+            """,
+            """
+            import reactor.core.observability.DefaultSignalListener;
+            import reactor.core.publisher.Mono;
+            import reactor.core.publisher.SignalType;
+            
+            class SomeClass {
+                void doSomething(Mono<String> mono) {
+                    mono.tap(() -> new DefaultSignalListener<>() {
+                                @Override
+                                public void doFinally(SignalType terminationType) {
+                                    System.out.println("other logs");
+                                }
+            
+                                @Override
+                                public void doOnNext(String result) {
+                                    System.out.println("success" + result);
                                 }
             
                                 @Override
